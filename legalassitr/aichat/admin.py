e0c9db11ea374d2a1eab87565e673edc54e6775a
ankur <dangerankur56@gmail.com>
2024-09-22 13:23:19 +0530

    Added Admin Models

--- legalassitr/aichat/admin.py
+++ legalassitr/aichat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from aichat.models import Conversation, DynamicForm, Message, UploadDocument
 
 # Register your models here.
+admin.site.register(UploadDocument)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(DynamicForm)
